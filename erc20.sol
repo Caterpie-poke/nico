a99945c9053f3dd255ff675f5f038589f489027d
yosuke <origami@caterpie.local>
2019-02-22 23:41:40 +0900

    fix output path

---
+++ erc20.sol
@@ -0,0 +1,155 @@
+pragma solidity ^0.4.24;
+
+library SafeMath {
+    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+        if (a == 0) {
+            return 0;
+        }
+        uint256 c = a * b;
+        require(c / a == b, 'Mul Err');
+        return c;
+    }
+    function div(uint256 a, uint256 b) internal pure returns (uint256) {
+        require(b > 0, 'Div Err');
+        uint256 c = a / b;
+        return c;
+    }
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+        require(b <= a, 'Sub Err');
+        uint256 c = a - b;
+        return c;
+    }
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a + b;
+        require(c >= a, 'Add Err');
+        return c;
+    }
+    function mod(uint256 a, uint256 b) internal pure returns (uint256) {
+        require(b != 0, 'Mod Err');
+        return a % b;
+    }
+    function exp(uint256 a, uint256 b) internal pure returns(uint256){
+        require(b >= 0, 'Exp Err');
+        uint256 c = 1;
+        for(uint256 i = 0 ; i < b ; i++){
+            c = mul(c, a);
+        }
+        return c;
+    }
+}
+
+contract c_00450052004300320030306b57fa3065304f4eee60f3901a8ca8306b95a23059308b59517d04 {
+    using SafeMath for uint256;
+
+    string internal v_4eee60f3901a8ca8540d;    /*仮想通貨の名称*/
+    uint256 internal v_7dcf767a884c91cf;    /*仮想通貨の総発行量*/
+    mapping(address=>uint256) internal map0;    /*人物Xの仮想通貨保有量*/
+    mapping(address=>mapping(address=>uint256)) internal map1;    /*人物Xが人物Yに許可した送金可能な仮想通貨量*/
+
+    /*
+    @function constructor 新規発行
+    @param v_63075b9a5024 仮想通貨の総発行量として指定された値
+    @param v_63075b9a540d79f0 仮想通貨の名称として指定された文字列
+    */
+    constructor(uint256 v_63075b9a5024, string memory v_63075b9a540d79f0) public {
+        v_7dcf767a884c91cf = v_63075b9a5024.mul(uint256(10)).exp(uint256(18));
+        v_4eee60f3901a8ca8540d = v_63075b9a540d79f0;
+        map0[msg.sender] = v_7dcf767a884c91cf;
+    }
+    /*
+    @event Transfer 送金の記録
+    @param v_900191d15143 送金元として指定された人物
+    @param v_900191d15148 送金先として指定された人物
+    @param v_900191d1984d 指定された仮想通貨量
+    */
+    event Transfer(address v_900191d15143, address v_900191d15148, uint256 v_900191d1984d);
+    /*
+    @event Approve 第三者による送金の許可の記録
+    @param v_900191d15143 送金者が送金可能な仮想通貨の保有者として指定された人物
+    @param v_900191d18005 送金の実行権の保有者として指定された人物
+    @param v_8a3153ef984d 送金者が送金可能とする指定された仮想通貨量
+    */
+    event Approve(address v_900191d15143, address v_900191d18005, uint256 v_8a3153ef984d);
+
+    /*
+    @function totalSupply 総発行量の確認
+    @return v_7dcf767a884c91cf 仮想通貨の総発行量
+    */
+    function totalSupply() public view returns(uint256){
+        return (v_7dcf767a884c91cf);
+    }
+
+    /*
+    @function f_4eee60f3901a8ca8540d306e78ba8a8d 仮想通貨名の確認
+    @return v_4eee60f3901a8ca8540d 仮想通貨の名称
+    */
+    function f_4eee60f3901a8ca8540d306e78ba8a8d() public view returns(string memory){
+        return (v_4eee60f3901a8ca8540d);
+    }
+
+    /*
+    @function balanceOf 残高の確認
+    @param v_5bfe8c618005 残高の確認を求める指定された人物
+    @return map0[v_5bfe8c618005] 残高の確認を求める指定された人物の残高
+    */
+    function balanceOf(address v_5bfe8c618005) public view returns(uint256){
+        require(v_5bfe8c618005 != address(0));
+        return (map0[v_5bfe8c618005]);
+    }
+
+    /*
+    @function allowance 送金可能な金額の確認
+    @param v_4fdd67098005 送金される仮想通貨の保有者として指定された人物
+    @param v_900191d18005 送金の実行者として指定された人物
+    @return map1[v_4fdd67098005][v_900191d18005] 送金される仮想通貨の保有者として指定された人物から送金の実行者として指定された人物が送金可能な額
+    */
+    function allowance(address v_4fdd67098005, address v_900191d18005) public view returns(uint256){
+        require(v_4fdd67098005 != address(0));
+        require(v_900191d18005 != address(0));
+        return (map1[v_4fdd67098005][v_900191d18005]);
+    }
+
+    /*
+    @function transfer 送金
+    @param v_900191d15148 送金先として指定された人物
+    @param v_900191d1984d 指定された仮想通貨量
+    */
+    function transfer(address v_900191d15148, uint256 v_900191d1984d) public returns(bool){
+        require(v_900191d15148 != address(0));
+        require(map0[msg.sender] >= v_900191d1984d);
+        map0[msg.sender] = map0[msg.sender].sub(v_900191d1984d);
+        map0[v_900191d15148] = map0[v_900191d15148].add(v_900191d1984d);
+        emit Transfer(msg.sender, v_900191d15148, v_900191d1984d);
+        return true;
+    }
+
+    /*
+    @function approve 第三者による送金の許可
+    @param v_900191d18005 送金の実行権の保有者として指定された人物
+    @param v_8a3153ef984d 送金者が送金可能とする指定された仮想通貨量
+    */
+    function approve(address v_900191d18005, uint256 v_8a3153ef984d) public returns(bool){
+        require(v_900191d18005 != address(0));
+        map1[msg.sender][v_900191d18005] = v_8a3153ef984d;
+        emit Approve(msg.sender, v_900191d18005, v_8a3153ef984d);
+        return true;
+    }
+
+    /*
+    @function transferFrom 第三者による送金
+    @param v_88ab900191d18005 送金される仮想通貨の保有者として指定された人物
+    @param v_900191d15148 送金先として指定された人物
+    @param v_900191d1984d 指定された仮想通貨量
+    */
+    function transferFrom(address v_88ab900191d18005, address v_900191d15148, uint256 v_900191d1984d) public returns(bool){
+        require(v_88ab900191d18005 != address(0));
+        require(v_900191d15148 != address(0));
+        require(map0[v_88ab900191d18005] >= v_900191d1984d);
+        require(map1[v_88ab900191d18005][msg.sender] >= v_900191d1984d);
+        map0[v_88ab900191d18005] = map0[v_88ab900191d18005].sub(v_900191d1984d);
+        map1[v_88ab900191d18005][msg.sender] = map1[v_88ab900191d18005][msg.sender].sub(v_900191d1984d);
+        map0[v_900191d15148] = map0[v_900191d15148].add(v_900191d1984d);
+        emit Transfer(v_88ab900191d18005, v_900191d15148, v_900191d1984d);
+        return true;
+    }
+}
